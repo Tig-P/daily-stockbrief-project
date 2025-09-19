169440424762d83702b12d41c88c631d39a9e9cb
Tig-P <enomme@naver.com>
2025-09-20 01:28:24 +0900

    좀 되라 통으로 올려보자

--- daily-stockbrief-web/.next/types/app/layout.ts
+++ daily-stockbrief-web/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\hae_w\Documents\daily-stockbrief-web\app\layout.tsx
+// File: C:\Users\hae_w\Documents\daily-stockbrief-project\daily-stockbrief-web\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- daily-stockbrief-web/.next/types/app/page.ts
+++ daily-stockbrief-web/.next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\hae_w\Documents\daily-stockbrief-web\app\page.tsx
+// File: C:\Users\hae_w\Documents\daily-stockbrief-project\daily-stockbrief-web\app\page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
